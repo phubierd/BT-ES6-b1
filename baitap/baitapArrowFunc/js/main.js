0f0f461983e4aaad132d83ee06cda41a96e874a7
PhuCT <123@123>
2021-04-27 14:41:01 +0700

    123

--- baitap/baitapArrowFunc/js/main.js
+++ baitap/baitapArrowFunc/js/main.js
@@ -1,70 +1,59 @@
-const colorList = ["pallet", "viridian", "pewter", "cerulean", "vermillion", "lavender", "celadon", "saffron", "fuschia", "cinnabar"];
-
-
-
-
+const colorList = [
+  "pallet",
+  "viridian",
+  "pewter",
+  "cerulean",
+  "vermillion",
+  "lavender",
+  "celadon",
+  "saffron",
+  "fuschia",
+  "cinnabar",
+];
 
 const hienThiDS = () => {
-    let content = "";
+  let content = "";
 
-    colorList.map((colorList) => {
-        content += `
-        <button class="color-button ${colorList}"   onclick='clicky()'></button>
-        `
-    });
+  colorList.forEach((item) => {
+    content += `
+        <button class="color-button ${item}" value="${item}" onclick='clickMe(value)'></button>
+        `;
+  });
 
-    document.getElementById("colorContainer").innerHTML = content;
-}
-
+  document.getElementById("colorContainer").innerHTML = content;
+};
 
 hienThiDS();
 
-// console.log(document.getElementById("colorContainer"))
 
 
+let listButton = document.querySelectorAll("button");
+// console.log(listButton)
 
+listButton.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log(btn);
+    // console.log(btn.getAttribute("value"))
+    btn.classList.add("active");
+    if (btn)
+      document.getElementById("house").classList.add(btn.getAttribute("value"));
+  });
+});
 
-// click đổi màu:
-
-
-// tryOn();
-// const tryOn = (id)=>{
-//     let colorSelected;
-
-//     for (let value of colorList){
-//         if (value.id == id){
-//             console.log(value);
-//             colorSelected = value;
-//         }
-//     }
-// }
-
-
-const clicky = () => {
-    console.log("123")
+const clickMe = (value) => {
+//   console.log(value);
     let colorSelected;
-
-    for (let value of colorList) {
-        console.log(value)
-        // if (document.querySelector("").getAttribute("class"))
-
-    }
-
-    // // for (let index in colorList){
-    // //     console.log(colorList[index])
-    // //     console.log(colorList)
-    // // }
-
-    // for(let i = 0 ; i< colorList.length; i++){
-    //     console.log(colorList[i])
+    // for (let i = 0; i< document.getElementById("colorContainer").length;i++){
+    //     console.log("i",getElementById("colorContainer")[i])
+    //     if (getElementById("colorContainer")[i].value == value){
+            
+    //         getElementById("colorContainer")[i].classList.add("active");
+    //     }else {
+    //         getElementById("colorContainer")[i].classList.remove("active")
+    //     }
     // }
 
-}
-window.clicky = clicky;
+};
+window.clickMe = clickMe;
 
-// const ppp = document.querySelector(".cinnabar").getAttribute("class")
-// console.log(ppp)
-// const www = document.querySelector(".color-button")
-// console.log(www)
-const qqq = document.querySelectorAll("button")[0].getAttribute("class")
-console.log(qqq)
+
